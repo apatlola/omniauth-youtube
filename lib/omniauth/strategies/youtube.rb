2ffe28c225e04a3e234e0c745d57e616743fbe15
Ashwini Krishna <ashwini.patlola@gmail.com>
2012-06-19 13:08:55 -0500

    Update master

--- lib/omniauth/strategies/youtube.rb
+++ lib/omniauth/strategies/youtube.rb
@@ -4,19 +4,27 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class YouTube < OmniAuth::Strategies::OAuth2
+      # Give your strategy a name.
       option :name, 'youtube'
+      
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
       option :client_options, {
         :site => 'https://www.youtube.com',
         :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
         :token_url => 'https://accounts.google.com/o/oauth2/token'
       }
       option :authorize_params, {
-        :scope => 'http://gdata.youtube.com'
+        :scope => 'http://gdata.youtube.com',
         :access_type => 'offline',
         :approval_prompt => 'force'
       }
-
-
+      
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
       uid { user['id']['$t'] }
 
       info do
