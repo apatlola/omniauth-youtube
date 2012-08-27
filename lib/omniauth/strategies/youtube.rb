01a8da9877ba5d844a8a00664ac5987b468da939
Ashwini Krishna <ashwini.patlola@gmail.com>
2012-08-26 19:29:58 -0500

    Update lib/omniauth/strategies/youtube.rb

--- lib/omniauth/strategies/youtube.rb
+++ lib/omniauth/strategies/youtube.rb
@@ -25,10 +25,13 @@ module OmniAuth
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
+      
+      p "==========before uid"
       uid { user['id']['$t'] }
 
       info do
         {
+          p "========with in info block"
           'uid' => user['id']['$t'],
           'nickname' => user['author'].first['name']['$t'],
           'first_name' => user['yt$firstName'] && user['yt$firstName']['$t'],
@@ -46,8 +49,15 @@ module OmniAuth
       def user
         user_hash['entry']
       end
+      
+      def build_access_token
+        verifier = request.params['code']
+        client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)))
+      end
 
       def user_hash
+        p "==============user hash"
+        p @access_token
         @user_hash ||= MultiJson.decode(@access_token.get("http://gdata.youtube.com/feeds/api/users/default?alt=json").body)
       end
 
